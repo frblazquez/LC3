1bf8073192bae68fd7dc5aa385da546175ce93d2
Francisco Javier Blazquez Martinez <frblazquezm@gmail.com>
2020-10-23 21:04:52 +0200

    Few preprocessing functions implementations added

--- Project 1/scripts/preprocessing.py
+++ Project 1/scripts/preprocessing.py
@@ -9,16 +9,37 @@
 
 import numpy as np
 
+# Removes features with high Pearson correlation coefficient
 def remove_highly_correlated_features(y, tx, threshold):
-    raise NotImplementedError
+    # First we compute the correlation coefficient of every pair
+    corr_coef = np.corrcoef(tx, y, False)
+    # Then we remove one feature of every pair of those with corr. coef. 
+    # greater than the threshold provided
+    highly_correlated_features = []
+    for i in range(corr_coef.shape[1]-1):
+        if i in highly_correlated_features:
+            continue
+        for j in range(i+1, corr_coef.shape[1]):
+            if j in highly_correlated_features:
+                continue
+            if abs(corr_coef[i][j]) > threshold:
+                # print('Removing '+str(j)+' for its correalation with '+str(i))
+                highly_correlated_features.append(j)
+    return np.delete(tx, highly_correlated_features, 1)
 
+# Removes features with missing values frequency greater than threshold
 def remove_features_with_MV(y, tx, MV, threshold):
-    raise NotImplementedError
+    return np.delete(tx,np.where(np.count_nonzero(tx == MV, axis=0)/tx.shape[0]>threshold),axis=1)
 
 def remove_points_with_MV(y, tx, MV):
-    raise NotImplementedError
-    
+    points_with_MV = np.where(np.count_nonzero(tx == MV, axis=1)>0)
+    tx = np.delete(tx, points_with_MV, axis=0)
+    #y  = np.delete(y,  points_with_MV, axis=0)
+    return tx #, y
+
 def replace_MV_by_average(y, tx, MV):
+    avgs = np.avg(remove_points_with_MV(y, tx, MV))
+    # Work in progress
     raise NotImplementedError
 
 def replace_MV_by_zero(y, tx, MV):
