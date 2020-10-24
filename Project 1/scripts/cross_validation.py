805b69d736ac09dae5eeef7e2957647530a23cd0
Francisco Javier Blazquez Martinez <frblazquezm@gmail.com>
2020-10-24 16:52:56 +0200

    Cross validation enviroment added, preprocessing functions almost finished

--- Project 1/scripts/cross_validation.py
+++ Project 1/scripts/cross_validation.py
@@ -2,13 +2,88 @@
 #
 # École polytechnique fédérale de Lausanne, Switzerland
 
-
-# IMPORTANT! The idea here is the following:
-# We will like like to decide the best hyper-parameters for our model. For
-# this purpose we will iterate over all possible combinations and obtain
-# an estimation of our model's precission by cross validation over our train
-# data splited in 5/10. 
-
 import numpy as np
 
+from proj1_helpers   import *
+from preprocessing   import *
+from implementations import *
 
+MV = -999
+
+# Hyper parameters for GD, SGD and least squares
+MISSING_VALUES_THRESHOLDS = [0.5]      #[0, 0.25,0.5,0.75]
+CORRELATION_THRESHOLDS    = [0.8,0.85] #[0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
+OUTLIERS_THRESHOLDS       = [3]        #[3,4,5,6]
+AUGMENTATION_DEGREES      = [2,3]      #[1,2,3,4,5]
+
+
+# Function to chose by cross validation the best hyper parameters for our model
+def train_least_squares(y, tx):
+    p_max = 0 
+
+    for mvt in MISSING_VALUES_THRESHOLDS:
+        for ct in CORRELATION_THRESHOLDS:
+            for ot in OUTLIERS_THRESHOLDS:
+                for ad in AUGMENTATION_DEGREES:
+                    # Here we have our data with some features dropped
+                    p = cross_validation(y, tx, 5, mvt, ct, ot, ad)
+                    print('Precission '+str(p)+' obtained for MVT='+str(mvt)+' CT='+str(ct)+' OT='+str(ot)+' AD='+str(ad)) 
+ 
+                    # Are we improving our best precission?
+                    if p > p_max:
+                        best_mvt = mvt
+                        best_ct  = ct
+                        best_ad  = ad
+                        p_max    = p  
+
+    return p_max, best_mvt, best_ct, best_ad   
+
+
+# Cross validation for concrete hyper parameters for our model
+def cross_validation(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2):
+    # Data random distribution in k sets
+    idxs = np.arange(tx.shape[0])
+    np.random.shuffle(idxs)
+    idxs_folds = np.split(idxs, k)
+    accuracies = []
+
+    for i in range(k):
+        # Data splitting in test and train set, it's important to copy them
+        tx_test  = tx[idxs_folds[i]].copy()
+        y_test   = y[ idxs_folds[i]].copy()
+        tx_train = tx[[j for j in idxs if j not in idxs_folds[i]]].copy()
+        y_train  = y[[ j for j in idxs if j not in idxs_folds[i]]].copy()
+
+        # Data preprocessing
+        corr_features = get_correlated_features(y_train, tx_train, ct)
+        tx_train = np.delete(tx_train, corr_features, axis=1)  
+        tx_test  = np.delete(tx_test,  corr_features, axis=1)    
+
+        high_MV_freq_features = get_high_MV_freq_features(y_train, tx_train, mvt)
+        tx_train = np.delete(tx_train, high_MV_freq_features, axis=1)
+        tx_test  = np.delete(tx_test,  high_MV_freq_features, axis=1)
+  
+        #replace_MV_by_average(y_train, tx_train)# Shouldn't we remove y argument from those functions not needing it?
+        #replace_MV_by_average(y_test,  tx_test) # Shouldn't we replace MV in test set with average in train set?
+
+        #y_train, tx_train = remove_outliers(y_train, tx_train,ot)
+
+        tx_train = feature_augmentation(y_train, tx_train, ad)
+        tx_test  = feature_augmentation(y_test,  tx_test,  ad)  
+      
+        tx_train = standarize(y_train, tx_train)
+        tx_test  = standarize(y_test,  tx_test)
+        
+        # Model train
+        w, loss     = least_squares(y_train, tx_train)
+
+        # Model test
+        y_test_pred = predict_labels(w, tx_test)
+        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0] # Sure there is a fancier way
+        #print(accuracy)
+
+        accuracies.append(accuracy)
+
+    return sum(accuracies)/len(accuracies)
+
+             
--- Project 1/scripts/preprocessing.py
+++ Project 1/scripts/preprocessing.py
@@ -2,15 +2,10 @@
 #
 # École polytechnique fédérale de Lausanne, Switzerland
 
-# TODO: Handle PRI_jet_num categorical variable!
-# TODO: Check there is no NaNs
-# TODO: Implement all the functions
-# TODO: Remove low variance features ? 
-
 import numpy as np
 
 # Removes features with high Pearson correlation coefficient
-def remove_highly_correlated_features(y, tx, threshold):
+def get_correlated_features(y, tx, threshold):
     # First we compute the correlation coefficient of every pair
     corr_coef = np.corrcoef(tx, y, False)
     # Then we remove one feature of every pair of those with corr. coef. 
@@ -25,38 +20,60 @@ def remove_highly_correlated_features(y, tx, threshold):
             if abs(corr_coef[i][j]) > threshold:
                 # print('Removing '+str(j)+' for its correalation with '+str(i))
                 highly_correlated_features.append(j)
-    return np.delete(tx, highly_correlated_features, 1)
+    return highly_correlated_features
 
 # Removes features with missing values frequency greater than threshold
-def remove_features_with_MV(y, tx, MV, threshold):
-    return np.delete(tx,np.where(np.count_nonzero(tx == MV, axis=0)/tx.shape[0]>threshold),axis=1)
+def get_high_MV_freq_features(y, tx, threshold, MV=-999):
+    return np.where(np.count_nonzero(tx == MV, axis=0)/tx.shape[0]>threshold)
 
 # Removes points in tx with missing values, it doesn't modify y!
-def remove_points_with_MV(y, tx, MV):
-    #points_with_MV = np.where(np.count_nonzero(tx == MV, axis=1)>0)
-    #tx = np.delete(tx, points_with_MV, axis=0)
-    #y  = np.delete(y,  points_with_MV, axis=0)
+def remove_points_with_MV(y, tx, MV=-999):
     return np.delete(tx, np.where(np.count_nonzero(tx == MV, axis=1)>0, axis=0))
 
 # Replaces every missing value entry with the average of valid values in that feature (modifies tx)
-def replace_MV_by_average(y, tx, MV):
+def replace_MV_by_average(y, tx, MV=-999):
     # NOT CORRECT!    
-    tx[tx == MV] = np.mean(remove_points_with_MV(y, tx, MV))
+    # tx[tx == MV] = np.mean(remove_points_with_MV(y, tx, MV))
+    raise NotImplementedError
 
 # Replaces every missing value entry with zero (modifies tx)
-def replace_MV_by_zero(y, tx, MV):
+def replace_MV_by_zero(y, tx, MV=-999):
     tx[tx == MV] = 0
 
 # Removes points deviated from the mean more than a std factor given
-def remove_outliers(y, tx, threshold):
-    # NOT CORRECT!
-    return tx[np.linalg.norm(tx - np.mean(tx,axis=0)) < threshold*np.std(tx)]
-    
+def remove_outliers(y, tx=None, threshold=4, ddof=0):    
+    # Calculate zscore
+    #xy = np.asanyarray(y.copy().drop(['Prediction'], axis=1))
+    #y_zscores = (xy - xy.mean(axis=0, keepdims=True)) / xy.std(axis=0, ddof=ddof, keepdims=True)
+    # Clean Outliers
+    #z_train = np.abs(y_zscores)
+    #return y[(z_train < threshold).all(axis=1)]
+    raise NotImplementedError
+
 # Z standarization of the data
 def standarize(y, tx):
     return (tx - np.mean(tx, axis=0)) / np.std(tx, axis=0)
 
-# 
-def feature_augmentation(y, tx):
-    raise NotImplementedError
+# Add new features to increase (partially) regression order
+def feature_augmentation(y, tx, k):
+    tx_aux = tx
+    # Here we should add the cross products also if k > 2! 
+    for i in range(2,k+1):
+        tx_aux = np.append(tx_aux, np.power(tx,i), axis=1)
+    return tx_aux
+        
+
+# Create this function from the preprocessing section in cross_validation.py
+#
+# def preprocess_data(y_train, tx_train, tx_test, mvt, ct, ot, ad):
+#        corr_features = get_correlated_features(y_train, tx_train, ct)
+#        tx_train = np.delete(tx_train, corr_features, axis=1)  
+#        tx_test  = np.delete(tx_test,  corr_features, axis=1)    
+#
+#        high_MV_freq_features = get_high_MV_freq_features(y_train, tx_train, mvt)
+#        tx_train = np.delete(tx_train, high_MV_freq_features, axis=1)
+#        tx_test  = np.delete(tx_test,  high_MV_freq_features, axis=1)
+#        .....
+
+    
 
