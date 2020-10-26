6b48e5bca80c7a5b1ced62791792f669932eed58
Francisco Javier Blazquez Martinez <frblazquezm@gmail.com>
2020-10-26 14:31:39 +0100

    final code review and run.py added

--- Project 1/scripts/cross_validation.py
+++ Project 1/scripts/cross_validation.py
@@ -1,6 +1,12 @@
-# Cross validation utilities for Machine Learning course, project 1
-#
 # École polytechnique fédérale de Lausanne, Switzerland
+# CS-433 Machine Learning, project 1
+# 
+# Francisco Javier Blázquez Martínez ~ francisco.blazquezmartinez@epfl.ch
+# David Alonso del Barrio            ~ david.alonsodelbarrio@epfl.ch
+# Andrés Montero Ranc                ~ andres.monteroranc@epfl.ch
+#
+# Functions for training and validating least squares, ridge regression,
+# logistic regression and regularized logistic regression models.
 
 import numpy as np
 
@@ -9,36 +15,116 @@ from preprocessing   import *
 from implementations import *
 
 # Hyper parameters for GD, SGD and least squares
-MISSING_VALUES_THRESHOLDS = [0.5]   #[0, 0.25,0.5,0.75]
-CORRELATION_THRESHOLDS    = [0.8]   #[0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
-OUTLIERS_THRESHOLDS       = [3]     #[3,4,5,6]
-AUGMENTATION_DEGREES      = [2]     #[1,2,3,4,5]
+MISSING_VALUES_THRESHOLDS = [1]
+CORRELATION_THRESHOLDS    = [1]
+OUTLIERS_THRESHOLDS       = [10]     
+AUGMENTATION_DEGREES      = [1,2,4]
 
+# Hyper parameters for LR and RLR
+REG_LOG_REG_LAMBDA = [1]              
+REG_LOG_REG_GAMMA  = [1e-7]   
 
-# Function to chose by cross validation the best hyper parameters for our model
+# Least squares grid search for hyper parameters with cross validation
 def train_least_squares(y, tx):
-    p_max = 0 
+    p_max = -1 
 
     for mvt in MISSING_VALUES_THRESHOLDS:
         for ct in CORRELATION_THRESHOLDS:
             for ot in OUTLIERS_THRESHOLDS:
                 for ad in AUGMENTATION_DEGREES:
-                    # Here we have our data with some features dropped
-                    p = cross_validation(y, tx, 5, mvt, ct, ot, ad)
+                    p = cross_validation_LS(y, tx, 10, mvt, ct, ot, ad)
                     print('Precission '+str(p)+' obtained for MVT='+str(mvt)+' CT='+str(ct)+' OT='+str(ot)+' AD='+str(ad)) 
  
-                    # Are we improving our best precission?
                     if p > p_max:
-                        best_mvt = mvt
-                        best_ct  = ct
-                        best_ad  = ad
-                        p_max    = p  
+                        p_max,best_mvt,best_ct,best_ot,best_ad = p,mvt,ct,ot,ad
 
-    return p_max, best_mvt, best_ct, best_ad   
+    return p_max, best_mvt, best_ct, best_ot, best_ad  
+
+# Ridge regression grid search for hyper parameters with cross validation
+def train_ridge_regression(y, tx):
+    p_max = -1 
+
+    for mvt in MISSING_VALUES_THRESHOLDS:
+        for ct in CORRELATION_THRESHOLDS:
+            for ot in OUTLIERS_THRESHOLDS:
+                for ad in AUGMENTATION_DEGREES:
+                    for lamb in LAMBDAS: 
+                        p = cross_validation_RR(y, tx, 10, mvt, ct, ot, ad,lamb)
+                        print('Precission '+str(p)+' obtained for MVT='+str(mvt)+' CT='+str(ct)+' OT='+str(ot)+' AD='+str(ad)+ ' LAMB='+str(lamb)) 
+ 
+                        if p > p_max:
+                            p_max,best_mvt,best_ct,best_ot,best_ad,best_lamb = p,mvt,ct,ot,ad,lamb 
+
+    return p_max, best_mvt, best_ct, best_ot, best_ad, best_lamb  
+
+# Logistic regression grid search for hyper parameters with cross validation
+def train_logistic_regression(y, tx):
+    p_max = -1 
+
+    for mvt in MISSING_VALUES_THRESHOLDS:
+        for ct in CORRELATION_THRESHOLDS:
+            for ot in OUTLIERS_THRESHOLDS:
+                for ad in AUGMENTATION_DEGREES:
+                    for gamma in REG_LOG_REG_GAMMA:
+                        p = cross_validation_LR(y, tx, 5, mvt, ct, ot, ad, gamma)
+                        print('Precission '+str(p)+' obtained for MVT='+str(mvt)+' CT='+str(ct)+' OT='+str(ot)+' AD='+str(ad)+ ' GAMMA='+str(gamma)) 
+
+                        if p > p_max:
+                            p_max,best_mvt,best_ct,best_ot,best_ad,best_gamma = p,mvt,ct,ot,ad,gamma  
+
+    return p_max, best_mvt, best_ct, best_ot, best_ad, best_gamma
+
+# Regularized logistic regression grid search for hyper parameters with cross validation
+def train_reg_logistic_regression(y, tx):
+    p_max = -1 
+
+    for mvt in MISSING_VALUES_THRESHOLDS:
+        for ct in CORRELATION_THRESHOLDS:
+            for ot in OUTLIERS_THRESHOLDS:
+                for ad in AUGMENTATION_DEGREES:
+                    for lamb in REG_LOG_REG_LAMBDA:
+                        for gamma in REG_LOG_REG_GAMMA:
+                            p = cross_validation_RLR(y, tx, 5, mvt, ct, ot, ad, lamb, gamma)
+                            print('Precission '+str(p)+' obtained for MVT='+str(mvt)+' CT='+str(ct)+' OT='+str(ot)+' AD='+str(ad)+' LAMBDA='+str(lamb)+' GAMMA='+str(gamma)) 
+
+                            if p > p_max:
+                                p_max,best_mvt,best_ct,best_ot,best_ad,best_lambda,best_gamma = p,mvt,ct,ot,ad,lamb,gamma 
+
+    return p_max, best_mvt, best_ct, best_ot, best_ad, best_lambda, best_gamma
 
 
-# Cross validation for concrete hyper parameters for our model
-def cross_validation(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2):
+# Leas squares k-fold corss validation of a model
+def cross_validation_LS(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2):
+    # Data random distribution in k sets
+    idxs = np.arange(tx.shape[0])
+    np.random.shuffle(idxs)
+    idxs_folds = np.array_split(idxs, k)
+    accuracies = []
+
+    for i in range(k):
+        # Data splitting in test and train set, it's important to copy them
+        tx_test  = tx[idxs_folds[i]].copy()
+        y_test   = y[ idxs_folds[i]].copy()
+        tx_train = tx[[j for j in idxs if j not in idxs_folds[i]]].copy()
+        y_train  = y[[ j for j in idxs if j not in idxs_folds[i]]].copy()
+
+        # Data preprocessing
+        y_train, tx_train, tx_test = preprocess_data(y_train, tx_train, tx_test, mvt, ct, ot, ad)
+        
+        # Model train
+        w, loss     = least_squares(y_train, tx_train)
+
+        # Model test
+        y_test_pred = predict_labels(w, tx_test)
+        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0]
+
+        accuracies.append(accuracy)
+
+    return sum(accuracies)/len(accuracies)
+
+
+# Ridge regression k-fold cross validation of a model
+def cross_validation_RR(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2, lamb=1):
     # Data random distribution in k sets
     idxs = np.arange(tx.shape[0])
     np.random.shuffle(idxs)
@@ -53,35 +139,75 @@ def cross_validation(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2):
         y_train  = y[[ j for j in idxs if j not in idxs_folds[i]]].copy()
 
         # Data preprocessing
-        corr_features = get_correlated_features(y_train, tx_train, ct)
-        tx_train = np.delete(tx_train, corr_features, axis=1)  
-        tx_test  = np.delete(tx_test,  corr_features, axis=1)    
+        y_train, tx_train, tx_test = preprocess_data(y_train, tx_train, tx_test, mvt, ct, ot, ad)
 
-        high_MV_freq_features = get_high_MV_freq_features(y_train, tx_train, mvt)
-        tx_train = np.delete(tx_train, high_MV_freq_features, axis=1)
-        tx_test  = np.delete(tx_test,  high_MV_freq_features, axis=1)
-  
-        replace_MV_by_average(y_train, tx_train)# Shouldn't we remove y argument from those functions not needing it?
-        replace_MV_by_average(y_test,  tx_test) # Shouldn't we replace MV in test set with average in train set?
-
-        y_train, tx_train = remove_outliers(y_train, tx_train,ot)
-
-        tx_train = feature_augmentation(y_train, tx_train, ad)
-        tx_test  = feature_augmentation(y_test,  tx_test,  ad)  
-      
-        tx_train = standarize(y_train, tx_train)
-        tx_test  = standarize(y_test,  tx_test)
-        
         # Model train
-        w, loss     = least_squares(y_train, tx_train)
+        w, loss     = ridge_regression(y_train, tx_train, lamb)
 
         # Model test
         y_test_pred = predict_labels(w, tx_test)
-        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0] # Sure there is a fancier way
-        #print(accuracy)
+        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0] 
 
         accuracies.append(accuracy)
 
     return sum(accuracies)/len(accuracies)
 
+# Logistic regression k-fold corss validation of a model
+def cross_validation_LR(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2, gamma=0.00001):
+    # Data random distribution in k sets
+    idxs = np.arange(tx.shape[0])
+    np.random.shuffle(idxs)
+    idxs_folds = np.split(idxs, k)
+    accuracies = []
+
+    for i in range(k):
+        # Data splitting in test and train set, it's important to copy them
+        tx_test  = tx[idxs_folds[i]].copy()
+        y_test   = y[ idxs_folds[i]].copy()
+        tx_train = tx[[j for j in idxs if j not in idxs_folds[i]]].copy()
+        y_train  = y[[ j for j in idxs if j not in idxs_folds[i]]].copy()
+
+        # Data preprocessing
+        y_train, tx_train, tx_test = preprocess_data(y_train, tx_train, tx_test, mvt, ct, ot, ad)
+        
+        # Model train
+        w, loss     = logistic_regression(y_train, tx_train, np.ones(tx_train.shape[1]), 100, gamma)
+
+        # Model test
+        y_test_pred = predict_labels(w, tx_test)
+        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0] 
+
+        accuracies.append(accuracy)
+
+    return sum(accuracies)/len(accuracies)
+
+
+# Regularized logistic regression k-fold corss validation of a model
+def cross_validation_RLR(y, tx, k, mvt=0.7, ct=0.85, ot=3, ad=2, lamb=10, gamma=0.00001):
+    # Data random distribution in k sets
+    idxs = np.arange(tx.shape[0])
+    np.random.shuffle(idxs)
+    idxs_folds = np.array_split(idxs, k)
+    accuracies = []
+
+    for i in range(k):
+        # Data splitting in test and train set, it's important to copy them
+        tx_test  = tx[idxs_folds[i]].copy()
+        y_test   = y[ idxs_folds[i]].copy()
+        tx_train = tx[[j for j in idxs if j not in idxs_folds[i]]].copy()
+        y_train  = y[[ j for j in idxs if j not in idxs_folds[i]]].copy()
+
+        # Data preprocessing
+        y_train, tx_train, tx_test = preprocess_data(y_train, tx_train, tx_test, mvt, ct, ot, ad)
+        
+        # Model train
+        w, loss     = reg_logistic_regression(y_train, tx_train, lamb, np.zeros(tx_train.shape[1]), 200, gamma)
+
+        # Model test
+        y_test_pred = predict_labels(w, tx_test)
+        accuracy    = len([a for (a,b) in zip(y_test,y_test_pred) if a == b])/tx_test.shape[0] 
+
+        accuracies.append(accuracy)
+
+    return sum(accuracies)/len(accuracies)
              
--- Project 1/scripts/implementations.py
+++ Project 1/scripts/implementations.py
@@ -1,6 +1,14 @@
-# Functions implementations for Machine Learning course, project 1
+# École polytechnique fédérale de Lausanne, Switzerland
+# CS-433 Machine Learning, project 1
+# 
+# Francisco Javier Blázquez Martínez ~ francisco.blazquezmartinez@epfl.ch
+# David Alonso del Barrio            ~ david.alonsodelbarrio@epfl.ch
+# Andrés Montero Ranc                ~ andres.monteroranc@epfl.ch
+#
+# Regresssion and classification functions implementations.
 
 import numpy as np
+
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
@@ -42,76 +50,18 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lamb):
+    # w is the solution to the system (Xt*X + lambda*I)w = Xt*y
     aI = lamb * np.identity(tx.shape[1])
     w  = np.linalg.solve(tx.T.dot(tx) + aI, tx.T.dot(y))
     err= y - tx.dot(w)
     loss = 1/2*np.mean(err**2)
     return w, loss
 
-def sigmoid(t): 
-    # Logistic function, equivalent to f(t) = e^t / (1 + e^t) 
-    return 1.0 / (1.0 + np.exp(-t)) # Reviewed
-
-
-def calculate_loss(y, tx, w):
-    """
-    compute the negative log likelihood for the logistic regression.
-
-    OUTPUTS: negative log likelihood
-    """
-
-    epsilon = 1e-5  
-    pred = sigmoid(tx.dot(w))
-    loss = y.T.dot(np.log(pred + epsilon)) + (1 - y).T.dot(np.log(1 - pred + epsilon)) 
-    return np.squeeze(- loss)
-
-def calculate_gradient(y, tx, w):
-    """compute the gradient, in a given point w, of the loss for the logistic regression.
-    INPUTS: y
-            X
-            w
-    OUTPUTS: the gradient vector
-    """
-    return tx.T.dot(sigmoid(tx.dot(w))-y)
-
-
-def learning_by_gradient_descent(y, tx, w, gamma):
-    """
-    Return the loss and the updated w.
-    INPUTS: y
-            X
-            w
-            gamma := the step size
-    OUTPUTS:w
-            loss
-    """
-    grad = calculate_gradient(y, tx, w)
-    loss = calculate_loss(y, tx, w)
-    w = w - gamma * grad
-    
-    return w, loss
-
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """
-    Logistic regression via gradient descent
-    INPUTS:
-            y
-            X
-            initial_w := the initialization of w for the algorithm
-            max_iters := max number of iterations
-            gamma := step size gamma
-            
-    
-    OUTPUTS:
-            w*, loss
-    """
     y_ = (y+1)/2
     w = initial_w
-    
     threshold = 1e-8
-
-    # init parameters
     losses = []
   
     # start the logistic regression
@@ -127,45 +77,11 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     
     return w, loss
 
-
-def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
-    """
-    Do one step of gradient descent, using the penalized logistic regression.
-    Return the loss and updated w.
-    """
-    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_)
-    w = w - gamma * gradient
-    return loss, w
-
-def penalized_logistic_regression(y, tx, w, lambda_):
-    """return the loss and gradient."""
-    
-    loss = calculate_loss(y, tx, w) + lambda_ * np.squeeze(w.T.dot(w))
-    gradient = calculate_gradient(y, tx, w) + 2 * lambda_ * w
-    return loss, gradient
-
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
-    """
-    Regularized logistic regression via gradient descent
-    INPUTS:
-            y
-            X
-            max_iter := max number of iterations
-            gamma := step size gamma
-            lambda := penalizing factor lambda
-            threshold := threshold for the update of the loss
-            
-    
-    OUTPUTS:
-            w*, loss
-    """
     y_ = (y+1)/2
-    threshold = 1e-8
-
-    # init parameters
-    losses = []
-
     w = initial_w
+    threshold = 1e-8
+    losses = []
 
     # start the logistic regression
     for iter in range(max_iters):
@@ -179,3 +95,45 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
             break
         
     return w, loss
+
+def sigmoid(t): 
+    # Logistic function, equivalent to f(t) = e^t / (1 + e^t) 
+    return 1.0 / (1.0 + np.exp(-t))
+
+
+def calculate_loss(y, tx, w):
+    # Logistic regression loss function
+    # epsilon for having safe logarithm!
+    epsilon = 1e-5  
+    pred = sigmoid(tx.dot(w))
+    loss = y.T.dot(np.log(pred + epsilon)) + (1 - y).T.dot(np.log(1 - pred + epsilon)) 
+    return np.squeeze(- loss)
+
+
+def calculate_gradient(y, tx, w):
+    # Gradient for logistic regression
+    return tx.T.dot(sigmoid(tx.dot(w))-y)
+
+
+def learning_by_gradient_descent(y, tx, w, gamma):
+    # Gradient descet schema
+    grad = calculate_gradient(y, tx, w)
+    loss = calculate_loss(y, tx, w)
+    w = w - gamma * grad
+    return w, loss
+
+
+def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
+    # Do one step of gradient descent, using the penalized logistic regression.
+    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_)
+    # Return the loss and updated w.
+    w = w - gamma * gradient
+    return loss, w
+
+
+def penalized_logistic_regression(y, tx, w, lambda_):
+    loss = calculate_loss(y, tx, w) + lambda_ * np.squeeze(w.T.dot(w))
+    gradient = calculate_gradient(y, tx, w) + 2 * lambda_ * w
+    return loss, gradient
+
+
